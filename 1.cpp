c6aa5db1d73a85a9c33f09f0ba9d34a6791ae057
RuiChenZhai <54008162+Malones314@users.noreply.github.com>
2021-08-15 19:47:20 +0800

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,34 @@
+//绘制七巧板https://codebus.cn/yangw/36-tasks-g1
+#include <graphics.h>		// 引用 EasyX 绘图库头文件
+#include <conio.h>
+
+int main ( ) {
+	initgraph ( 640, 720 );	// 创建绘图窗口，分辨率 640x480
+	POINT ptr [ ] = { { 100, 100 }, { 500, 100 }, { 500, 500 }, { 100, 500 } };
+	polygon ( ptr, 4 );   //描绘正方形
+	line ( 100, 100, 300, 300 );
+	line ( 100, 500, 500, 100 );
+	line ( 400, 200, 500, 300 );
+	line ( 500, 300, 300, 500 );
+	line ( 200, 400, 400, 400 );
+	line ( 400, 400, 300, 300 );
+	setfillcolor ( RED );
+	floodfill ( 200, 101, WHITE );   //从点向外扩散，遇到遇到白色停止扩散;
+	setfillcolor ( GREEN );
+	floodfill ( 110, 120, WHITE );
+	setfillcolor ( YELLOW );
+	floodfill ( 450, 200, WHITE );
+	setfillcolor ( RGB ( 100, 100, 100 ) );
+	floodfill ( 480, 480, WHITE );
+	setfillcolor ( RGB ( 200, 200, 200 ) );
+	floodfill ( 400, 300, WHITE );
+	setfillcolor ( RGB ( 100, 100, 0 ) );
+	floodfill ( 300, 350, WHITE );
+	setfillcolor ( RGB ( 100, 200, 100 ) );
+	floodfill ( 200, 450, WHITE );
+	setfillcolor ( RGB ( 250, 250, 250 ) );
+	floodfill ( 1, 1, WHITE );
+	_getch ( );				//按任意键继续
+	closegraph ( );			//关闭图形界面
+	return 0;
+}
